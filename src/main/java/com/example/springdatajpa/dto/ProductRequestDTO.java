ebb17dd78d16bd1d55ec5c7b0dee6ede5cc99d58
Angielyn Rivera <2194059@cognizant.com>
2023-03-06 15:26:20 +0800

    Added validations

--- src/main/java/com/example/springdatajpa/dto/ProductRequestDTO.java
+++ src/main/java/com/example/springdatajpa/dto/ProductRequestDTO.java
@@ -1,14 +1,19 @@
 package com.example.springdatajpa.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ProductRequestDTO {
 
-    private Integer id;
-
+    @NotNull(message = "Product name shoudn't be null")
     private String name;
+
+    @NotBlank(message = "Product description shoudn't be blank")
     private String description;
+
+    @NotNull(message = "Product price shoudn't be null")
     private Double price;
 
 }
